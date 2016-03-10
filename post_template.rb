8efe6b871c19166d709acb5d967652deb0d56162
phchan9 <pandoratx@gmail.com>
2016-03-10 23:34:20 +0800

    add category support for post_template.rb

--- post_template.rb
+++ post_template.rb
@@ -2,6 +2,8 @@
 
 require 'colorize'
 
+print "input category: "
+input_category_name = gets.chomp.downcase
 print "input topic name: "
 input_topic_name = gets.chomp
 
@@ -9,6 +11,9 @@ file_title = input_topic_name.split.map { |word| word.capitalize }.join(" ")
 file_name = input_topic_name.gsub(/\s+/m,"-") + ".md"
 puts "new file: " + file_name.green + " with title: " + file_title.blue
 
-File.open(file_name, "w") do |md_file|
+file_path = input_category_name + "/" + file_name
+
+File.open(file_path, "w") do |md_file|
   md_file.write("# #{file_title}\n\n# Reference\n")
 end
+puts "write template post into path: " + file_path.bold
